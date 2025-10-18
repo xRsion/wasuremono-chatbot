545b64e5f215967630dd77c3a96f81d898d2a6cd
ryosuke-kubota <kubota.development@gmail.com>
2025-10-18 15:22:58 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
+  trailingSlash: true,
   allowedDevOrigins: ["*.preview.same-app.com"],
   images: {
     unoptimized: true,
